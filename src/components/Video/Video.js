0e095a33a340bee2eb196c3742bb8bf50619c50d
babakdorani <babak.dorani@gmail.com>
2021-09-08 09:37:03 +0300

    video and categoriesBar components added to components folder

---
+++ src/components/Video/Video.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./_video.scss"
+function Video() {
+    return (
+        <div>
+                    Vidoe components             
+        </div>
+    )
+}
+    
+export default Video
---
+++ src/components/categoriesBar/CategoriesBar.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./_categoriesBar.scss";
+
+const CategoriesBar = () => {
+  return <div>Categories bar</div>;
+};
+
+export default CategoriesBar;
