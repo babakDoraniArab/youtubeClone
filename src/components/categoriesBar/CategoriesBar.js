7d75f4696fc72a7a05d5878390588313be27801d
babakdorani <babak.dorani@gmail.com>
2021-09-08 16:42:22 +0300

    CategoriesBar section done

--- src/components/categoriesBar/CategoriesBar.js
+++ src/components/categoriesBar/CategoriesBar.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "./_categoriesBar.scss";
 
 const CategoriesBar = () => {
-  return <div>Categories bar</div>;
+  const keyword = [
+    "All",
+    "React js",
+    " Angular Js",
+    "React Native",
+    "use of API",
+    "Redux",
+    "Music",
+    "Algorithm Art",
+    "Guitar",
+    "Bengali Songs",
+    "Coding",
+    "guitar",
+    "Gatsby",
+    "Real Madrid",
+    "piano",
+    "dance",
+    "Node js",
+    "programming",
+  ];
+  const [activeElement, setActiveElement] = useState("All");
+  const handleClick = (value) => setActiveElement(value);
+
+  return (
+    <div className="categoriesBar">
+      {keyword.map((value, index) => (
+        <span
+          key={index}
+          onClick={() => handleClick(value)}
+          className={activeElement === value ? "active" : " "}
+        >
+          {value}
+        </span>
+      ))}
+    </div>
+  );
 };
 
 export default CategoriesBar;
