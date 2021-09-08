21c8f5d87b9b9cfe10758e444bf66fcdb4af0d8d
babakdorani <babak.dorani@gmail.com>
2021-09-08 11:12:44 +0300

    Header scss done. header responsive done for now

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import "./_header.scss"
+import React from "react";
+import "./_header.scss";
+
+import { FaBars } from "react-icons/fa";
+import { AiOutlineSearch } from "react-icons/ai";
+import { MdNotifications, MdApps } from "react-icons/md";
 
 const Header = () => {
-    return (
-        <div className="border border-dark">
-                    header
-        </div>
-    )
-}
+  return (
+    <div className="border border-dark header">
+      <FaBars className="header__menu" size={26} />
+      <img
+        src="http://pngimg.com/uploads/youtube/youtube_PNG2.png"
+        alt="logo"
+        className="header__logo"
+      />
+      <form>
+        <input type="text" placeholder="Search" />
+        <button type="submit">
+          <AiOutlineSearch size={22} />
+        </button>
+      </form>
+      <div className="header__icons">
+        <MdNotifications size={28} />
+        <MdApps size={28} />
+        <img src="/avatar.png" alt="avatar" className="header__avatar"/>
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
