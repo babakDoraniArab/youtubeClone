dade965d155a29aedaab72d0151f409813334a0e
babakdorani <babak.dorani@gmail.com>
2021-09-08 09:33:04 +0300

    making basic components

---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./_header.scss"
+
+const Header = () => {
+    return (
+        <div>
+                    header
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/sidebar/Sidebar.js
@@ -0,0 +1,7 @@
+import React from "react";
+import "./_sidebar.scss";
+const Sidebar = () => {
+  return <div>sodebar</div>;
+};
+
+export default Sidebar;
