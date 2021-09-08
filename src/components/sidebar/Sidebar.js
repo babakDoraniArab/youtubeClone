416448d9f4a780323f4d23ed63e71a373f30575b
babakdorani <babak.dorani@gmail.com>
2021-09-08 11:53:01 +0300

    Sidebar.js jsx done Sidebar css done . header height changed form 17vh to 10 vh .

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,7 +1,52 @@
 import React from "react";
 import "./_sidebar.scss";
+
+import {
+  MdSubscriptions,
+  MdExitToApp,
+  MdThumbUp,
+  MdHistory,
+  MdLibraryBooks,
+  MdHome,
+  MdSentimentDissatisfied,
+} from "react-icons/md";
+
 const Sidebar = () => {
-  return <div className="border border-danger ">sodebar</div>;
+  return (
+    <nav className="border border-danger sidebar ">
+      <li>
+        <MdHome size={23} />
+        <span>Home</span>
+      </li>
+      <li>
+        <MdSubscriptions size={23} />
+        <span>Subscriptions</span>
+      </li>
+      <li>
+        <MdThumbUp size={23} />
+        <span>Liked Video</span>
+      </li>
+      <li>
+        <MdHistory size={23} />
+        <span>History</span>
+      </li>
+
+      <li>
+        <MdLibraryBooks size={23} />
+        <span>Library</span>
+      </li>
+      <li>
+        <MdSentimentDissatisfied size={23} />
+        <span>I don't know</span>
+      </li>
+      <hr />
+      <li>
+        <MdExitToApp size={23} />
+        <span>Log Out</span>
+      </li>
+      <hr />
+    </nav>
+  );
 };
 
 export default Sidebar;
