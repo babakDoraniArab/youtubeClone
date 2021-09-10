b400f2fbd3d3b49c0b6e30c4e36d9db082dd9c7f
babakdorani <babak.dorani@gmail.com>
2021-09-10 10:55:15 +0300

    ReactDom installed React router and Switch and all Routing stuff done and tested

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from "react-router-dom";
 
 import { Container } from "react-bootstrap";
 import CategoriesBar from "./components/categoriesBar/CategoriesBar";
@@ -6,10 +12,9 @@ import Header from "./components/header/Header";
 import HomeScreen from "./screen/homeScreen/HomeScreen";
 import Sidebar from "./components/sidebar/Sidebar";
 import Video from "./components/Video/Video";
-import "./_app.scss";
+import "./_app.scss"; 
 import LoginScreen from "./screen/loginScreen/LoginScreen";
-
-function App() {
+const Layout = ({ children }) => {
   const [sidebar, setSidebar] = useState(false);
 
   const handleToggleSidebar = () => setSidebar(!sidebar);
@@ -19,12 +24,36 @@ function App() {
       <div className="app__container ">
         <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
         <Container fluid className="app__main ">
-          <HomeScreen />
+          {/* <HomeScreen /> */}
+          {children}
         </Container>
       </div>
-
-      {/* <LoginScreen /> */}
     </>
+  );
+};
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Layout>
+            <HomeScreen />
+          </Layout>
+        </Route>
+        <Route path="/auth">
+          <LoginScreen />
+        </Route>
+        <Route path="/search" exact>
+          <Layout>
+            <h1>searchme</h1>
+          </Layout>
+        </Route>
+
+        <Route>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
