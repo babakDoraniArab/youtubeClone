bde228c2881e2b8751786d5b843388fc2c960984
babakdorani <babak.dorani@gmail.com>
2021-09-10 11:23:22 +0300

    let start with redux I installed 4 packages sudo npm i redux react-redux redux-thunk redux-devtools-extension
    redux store.js is done

---
+++ src/redux/store.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { thunk } from "redux-thunk";
+
+const initialState = {
+  name: "babak",
+  age: "32",
+};
+const reducer = (initialState) => initialState;
+
+const store = createStore(
+  reducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+// first of all you need to create initialState and reducer .after that  you need to import redux-thunk and redux dev-tools for
+// applying the middleware and redux thunk also you have to import them too
+
+export default store;
