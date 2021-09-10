02420348e8ebcd47946e2a7be42277cdf2a56502
babakdorani <babak.dorani@gmail.com>
2021-09-10 11:06:09 +0300

    I have done firebase part in the firebase website
    firebase package installed with npm i firebase
    
    firebase-tools installed with npm install -g firebase-tools
    
    so please install both of them when you reading this code

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAU-VDyU5uGIEwNOfN21zFuQOoKGxYkZvQ",
+  authDomain: "ytb-bablido.firebaseapp.com",
+  projectId: "ytb-bablido",
+  storageBucket: "ytb-bablido.appspot.com",
+  messagingSenderId: "854650259615",
+  appId: "1:854650259615:web:3c1cc6c5764adc12c7faaa",
+};
+
+firebase.initializeApp(firebaseConfig);
